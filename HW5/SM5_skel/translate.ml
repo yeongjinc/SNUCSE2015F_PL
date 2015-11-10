dcc44009e10e1d0c75b48ba22c7f53a7f2717990
Yeongjin Choi <yeongjinc@gmail.com>
2015-11-10 23:22:13 +0900

    for/while 제외 구현, 재귀 및 write output 등 처리

--- HW5/SM5_skel/translate.ml
+++ HW5/SM5_skel/translate.ml
@@ -1,5 +1,5 @@
 (*
- * SNU 4190.310 Programming Languages 
+ * SNU 4190.310 Programming Languages
  * K-- to SM5 translator skeleton code
  * Jaeseung Choi (jschoi@ropas.snu.ac.kr)
  *)
@@ -11,10 +11,42 @@ module Translator = struct
   (* TODO : complete this function  *)
   let rec trans : K.program -> Sm5.command = function
     | K.NUM i -> [Sm5.PUSH (Sm5.Val (Sm5.Z i))]
+    | K.TRUE -> [Sm5.PUSH (Sm5.Val (Sm5.B true))]
+    | K.FALSE -> [Sm5.PUSH (Sm5.Val (Sm5.B false))]
+    | K.UNIT -> [Sm5.PUSH (Sm5.Val Sm5.Unit)]
+    | K.VAR id -> [Sm5.PUSH (Sm5.Id id); Sm5.LOAD]
     | K.ADD (e1, e2) -> trans e1 @ trans e2 @ [Sm5.ADD]
+    | K.SUB (e1, e2) -> trans e1 @ trans e2 @ [Sm5.SUB]
+    | K.MUL (e1, e2) -> trans e1 @ trans e2 @ [Sm5.MUL]
+    | K.DIV (e1, e2) -> trans e1 @ trans e2 @ [Sm5.DIV]
+    | K.EQUAL (e1, e2) -> trans e1 @ trans e2 @ [Sm5.EQ]
+    | K.LESS (e1, e2) -> trans e1 @ trans e2 @ [Sm5.LESS]
+    | K.NOT e -> trans e @ [Sm5.NOT]
+     (* LOAD 시 (x, w)가 first such entry이므로 unbind 는 여기서 필요 없다 *)
+    | K.ASSIGN (id, e) -> trans e @ [Sm5.PUSH (Sm5.Id id); Sm5.STORE; Sm5.PUSH (Sm5.Id id); Sm5.LOAD]
+    | K.SEQ (e1, e2) -> trans e1 @ [Sm5.POP] @ trans e2
+    | K.IF (e1, e2, e3) -> trans e1 @ [Sm5.JTR ((trans e2), (trans e3))]
+    | K.WHILE (e1, e2) ->
+            let strWhile = "#while" in
+            []
+            (*trans e1 @ [Sm5.JTR ((trans e2 @ [Sm5.POP] @ trans(K.WHILE(e1, e2))), (Sm5.PUSH (Sm5.Val Sm5.Unit)))]*)
+    | K.FOR(id, e1, e2, e3) -> [](* TODO *)
     | K.LETV (x, e1, e2) ->
       trans e1 @ [Sm5.MALLOC; Sm5.BIND x; Sm5.PUSH (Sm5.Id x); Sm5.STORE] @
       trans e2 @ [Sm5.UNBIND; Sm5.POP]
+    | K.LETF (f, x, e1, e2) ->
+            [Sm5.PUSH (Sm5.Fn (x, [Sm5.BIND f] @ trans e1)); Sm5.BIND f] @ trans e2 @ [Sm5.UNBIND; Sm5.POP]
+    | K.CALLV (f, e) ->
+            [Sm5.PUSH (Sm5.Id f); Sm5.PUSH (Sm5.Id f)] @ trans e @ [Sm5.MALLOC; Sm5.CALL]
+    | K.CALLR (f, x) ->
+            [Sm5.PUSH (Sm5.Id f); Sm5.PUSH (Sm5.Id f); Sm5.PUSH (Sm5.Id x); Sm5.LOAD; Sm5.PUSH (Sm5.Id x); Sm5.CALL]
     | K.READ x -> [Sm5.GET; Sm5.PUSH (Sm5.Id x); Sm5.STORE; Sm5.PUSH (Sm5.Id x); Sm5.LOAD]
-    | _ -> failwith "Unimplemented"
+    | K.WRITE e ->
+            let t = "#temp" in
+            trans e @
+            [Sm5.MALLOC; Sm5.BIND t; Sm5.PUSH (Sm5.Id t); Sm5.STORE;
+            Sm5.PUSH (Sm5.Id t); Sm5.LOAD;
+            Sm5.PUSH (Sm5.Id t); Sm5.LOAD;
+            Sm5.PUT; Sm5.UNBIND; Sm5.POP]
+    (*| _ -> failwith "Unimplemented"*)
 end
